71bd4a19c8f028942fb47bd55a9e42dc2be4017d
Noelia Alcaraz <noeliarocioalcaraz@gmail.com>
2022-04-05 23:50:44 -0300

    calcular pagos en cuotas sobre un monto determinado

--- main.js
+++ main.js
@@ -1,6 +1,37 @@
-let numero = parseInt(prompt('Introduzca un numero'));
+//Calcular pagos en cuotas sobre un monto determinado
 
-for (let i = 0; i < 10; i++) {
-    numero += 2;
-    alert(numero);
+
+function calcularCuotas() {
+
+    let importeTotal = parseInt(prompt('Ingrese el importe total a calcular'));
+
+    while (isNaN(importeTotal)) {
+
+        importeTotal = prompt('El dato que ingreso no es valido. Ingrese el importe total a calcular')
+        
+    }
+
+    let cantidadCuotas = parseInt(prompt('¿Quiere aplicar 3, 6 o 12 cuotas sin interes?'));
+    let resultadoCuotas = importeTotal / cantidadCuotas;
+
+    if (cantidadCuotas === 3) {
+
+        alert(`Usted pagará $${importeTotal} en ${cuotas} cuotas de $${resultadoCuotas}`)
+
+    } else if (cantidadCuotas === 6) {
+
+        alert(`Usted pagará $${importeTotal} en ${cuotas} cuotas de $${resultadoCuotas}`)
+
+    } else if (cantidadCuotas === 12) {
+
+        alert(`Usted pagará $${importeTotal} en ${cuotas} cuotas de $${resultadoCuotas}`)
+
+    } else if (cantidadCuotas != 3 && cantidadCuotas != 6 && cantidadCuotas != 12) {
+
+        alert('la cantidad de cuotas no es válida')
+
+    }
 }
+
+calcularCuotas()
+
